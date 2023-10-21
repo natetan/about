a4b38e6d7e42e0ef07f35b98879709b5bb134cfc
Nate Tan <nytan67@gmail.com>
2023-10-21 11:36:32 -0700

    Updates

---
+++ precache-manifest.b7e34408c0b9d02b062c555de1e1e74b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce2b152e26779cc08c7fed05426d3ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7bf5488409928dba61a",
+    "url": "/static/css/main.c9b68960.chunk.css"
+  },
+  {
+    "revision": "a0be562ddf92932d57ef",
+    "url": "/static/js/2.991fccce.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/static/js/2.991fccce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7bf5488409928dba61a",
+    "url": "/static/js/main.17a42c41.chunk.js"
+  },
+  {
+    "revision": "6a5481713919289d0c2b",
+    "url": "/static/js/runtime-main.b0782015.js"
+  },
+  {
+    "revision": "d3b4a514dfb172f6ac1817f5bdb974ad",
+    "url": "/static/media/bg-masthead.d3b4a514.jpg"
+  },
+  {
+    "revision": "1f210aebf6f8c5406ce6b8df9081ddd0",
+    "url": "/static/media/ny-library-cropped.1f210aeb.png"
+  },
+  {
+    "revision": "08c33bc2895944979cba60a2cff6cecd",
+    "url": "/static/media/tech-resume.08c33bc2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9602dd38e62cb8c6f1e6f5592132f9f7.js"
+  "/precache-manifest.b7e34408c0b9d02b062c555de1e1e74b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
